dd7b93b42fd1be1bdd5f5e7e0852c1b90b7d30bd
jennananas <100166354+jennananas@users.noreply.github.com>
2023-11-07 16:48:45 +0100

    vite deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pkg from 'file:///Users/mac/Desktop/github/pokememory/node_modules/vite-plugin-gh-pages/dist/index.js';
+const { vitePlugin: VitePluginGhPages } = pkg
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  "include":  ["src", "styled-system"]
+  plugins: [react(), VitePluginGhPages],
+  "include":  ["src", "styled-system"],
+  "base": '/jennananas/pokememory/'
+  
 })
